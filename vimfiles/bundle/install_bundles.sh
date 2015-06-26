3c4eb2e736071b4c2819ea9536119c249359abf6
Thijs Brobbel <thijsbrobbel@gmail.com>
2015-06-26 10:31:36 +0200

    Add list of possible bundles.

--- vimfiles/bundle/install_bundles.sh
+++ vimfiles/bundle/install_bundles.sh
@@ -2,3 +2,11 @@
 
 git clone git@github.com:tpope/vim-sensible.git
 git clone git@github.com:tpope/vim-unimpaired.git
+
+# vim-colors-solarized
+# nerdcommenter
+# ctrlp.vim
+# gundo.vim
+# vim-fugitive
+# vim-dispatch
+# vim-surround
