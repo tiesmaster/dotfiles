5f1d60f7c0d1acf2a69c4157c9df4adbd91d6a68
Thijs Brobbel <thijsbrobbel@gmail.com>
2015-06-26 11:21:42 +0200

    Install ctrlp.vim, as bundle.

--- vimfiles/bundle/install_bundles.sh
+++ vimfiles/bundle/install_bundles.sh
@@ -10,8 +10,8 @@ install_bundle git@github.com:tpope/vim-sensible.git
 install_bundle git@github.com:tpope/vim-unimpaired.git
 install_bundle git@github.com:altercation/vim-colors-solarized.git
 install_bundle git@github.com:scrooloose/nerdcommenter.git
+install_bundle git@github.com:kien/ctrlp.vim.git
 
-# ctrlp.vim
 # gundo.vim
 # vim-fugitive
 # vim-dispatch
